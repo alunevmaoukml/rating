c71d35c9adad493c6f9dd0805359f8148ff0e42a
Alexander <ddimasss@gmail.com>
2016-08-03 09:31:59 +0000

    second commit

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=edusec',
+    'username' => 'edusec',
+    'password' => 'edusec',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true,
+];
